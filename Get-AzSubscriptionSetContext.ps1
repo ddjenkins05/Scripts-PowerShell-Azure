a81d697526bf51b4e8f7649b298edd86378caf47
ddjenkins05 <35473538+ddjenkins05@users.noreply.github.com>
2026-03-17 07:00:06 -0700

    Add files via upload

---
+++ Get-AzSubscriptionSetContext.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+Two simple Azure PowerShell scripts in one file:
+  1) List subscriptions (Name, Id, State)
+  2) Set the active subscription context
+
+.DESCRIPTION
+Script 1: Uses Get-AzSubscription to show Name, Id, and State.
+Script 2: Uses Set-AzContext to set the current session context to a subscription.
+
+.NOTES
+Requires Az.Accounts (part of the Az module).
+#>
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+#region Script 1 - List subscriptions
+# Lists subscriptions the current account can access.
+Get-AzSubscription | Select-Object Name, Id, State
+#endregion
+
+#region Script 2 - Set subscription context
+# Example usage:
+#   Set-AzContext -SubscriptionId "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+# or
+#   Set-AzContext -SubscriptionName "MyDevelopmentSubscription"
+
+# Uncomment ONE of the following lines and provide your value:
+# Set-AzContext -SubscriptionId "PUT-SUBSCRIPTION-GUID-HERE"
+# Set-AzContext -SubscriptionName "PUT-SUBSCRIPTION-NAME-HERE"
+#endregion
